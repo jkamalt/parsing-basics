2501cdf39a3be0c94b2a2777ae77205ddc8691a4
Julia Kamaltdinova <kamaltjf@mail.ru>
2022-04-04 16:39:26 +0500

    Добавляет исходный вариант парсинга для паука hhru

--- lesson6/jobparser/pipelines.py
+++ lesson6/jobparser/pipelines.py
@@ -6,8 +6,20 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from pymongo import MongoClient
 
 
 class JobparserPipeline:
+
+    def __init__(self):
+        client = MongoClient('localhost', 27017)
+        self.db_mongo = client.vacancy_db
+
     def process_item(self, item, spider):
+        # item['min'], item['max'], item['cur'] = self.process_salary(item['salary'])
+        collection = self.db_mongo[spider.name]
+        collection.insert_one(item)
         return item
+
+    def process_salary(self, salary):
+        return 1, 2, 3
