337096f2a3ed8cd964b7d79b634278013fb5950c
Julia Kamaltdinova <kamaltjf@mail.ru>
2022-04-08 17:52:56 +0500

    Исправляет настройки settings.py

--- lesson7/castoramaparser/settings.py
+++ lesson7/castoramaparser/settings.py
@@ -7,31 +7,37 @@
 #     https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
 #     https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 
+import config.config as user_config
+
 BOT_NAME = 'castoramaparser'
 
 SPIDER_MODULES = ['castoramaparser.spiders']
 NEWSPIDER_MODULE = 'castoramaparser.spiders'
 
+LOG_ENABLE = True
+LOG_LEVEL = 'DEBUG'
+
+IMAGES_STORE = 'photos'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'castoramaparser (+http://www.yourdomain.com)'
+USER_AGENT = user_config.USER_AGENT
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-#CONCURRENT_REQUESTS = 32
+CONCURRENT_REQUESTS = 8
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 1.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
 
 # Disable cookies (enabled by default)
-#COOKIES_ENABLED = False
+COOKIES_ENABLED = False
 
 # Disable Telnet Console (enabled by default)
 #TELNETCONSOLE_ENABLED = False
@@ -62,9 +68,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'castoramaparser.pipelines.CastoramaparserPipeline': 300,
-#}
+ITEM_PIPELINES = {
+   'castoramaparser.pipelines.CastoramaparserPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
